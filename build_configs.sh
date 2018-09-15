54c987b1e598cf2c577a9ccf72a97cd293ced0a7
ChrBarth <ChrBarth@users.noreply.github.com>
2018-09-15 14:24:55 +0200

    re-added TMP_RANGER and TMP_BASH in build_configs
    the script can now be run from anywhere, temp files
    will always be created in the home directory
    and deleted after creation of .bashrc and rc.conf

--- build_configs.sh
+++ build_configs.sh
@@ -5,6 +5,8 @@
 SHORTCUTSFILE="${HOME}/.config/folder_shortcuts.txt"
 BASHRC_BASE="${HOME}/.config/bashrc_base"
 RANGER_BASE="${HOME}/.config/ranger_base.conf"
+TMP_RANGER="${HOME}/ranger_temp"
+TMP_BASH="${HOME}/bash_temp"
 
 # first, check if source files exist:
 if [ ! -f $SHORTCUTSFILE ]
@@ -21,18 +23,18 @@ then
     exit 1
 fi
 
-echo -e "\n# folder mappings:" > ranger_mappings.tmp
-echo -e "\n# folder aliases:" > bash_aliases.tmp
+echo -e "\n# folder mappings:" > $TMP_RANGER
+echo -e "\n# folder aliases:" > $TMP_BASH
 
 # parse SHORTCUTSFILE and create temporary config files:
 # using <(cat... so we can use single-quotes in aliases for .bashrc:
 cat $SHORTCUTSFILE | \
-awk -f <(cat - <<-'_EOT_'
+awk -v RANGER=$TMP_RANGER -v BASH=$TMP_BASH -f <(cat - <<-'_EOT_'
 BEGIN{FS="\t"}
 /^[^#]/ { 
-    printf("map g%s cd %s\n", $1, $2) >> "ranger_mappings.tmp" ;
+    printf("map g%s cd %s\n", $1, $2) >> RANGER ;
     gsub(" ","\\ ",$2);
-    printf("alias %s='cd %s'\n",$1,$2) >> "bash_aliases.tmp" }
+    printf("alias %s='cd %s'\n",$1,$2) >> BASH }
 _EOT_
 )
 
@@ -44,10 +46,10 @@ then
 fi
 
 # create ~/.bashrc
-cat $BASHRC_BASE bash_aliases.tmp > ${HOME}/.bashrc
+cat $BASHRC_BASE $TMP_BASH > ${HOME}/.bashrc
 echo "" >> ${HOME}/.bashrc
 # create /.config/ranger/rc.conf
-cat $RANGER_BASE ranger_mappings.tmp > ${HOME}/.config/ranger/rc.conf
+cat $RANGER_BASE $TMP_RANGER > ${HOME}/.config/ranger/rc.conf
 
-rm ranger_mappings.tmp
-rm bash_aliases.tmp
+rm $TMP_RANGER
+rm $TMP_BASH
